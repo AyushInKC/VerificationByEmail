7f711ad31ea4ccecb2fb40dc96c4510249f878ca
AyushInKC <sirfayush1008@gmail.com>
2024-12-03 21:51:38 +0530

    Nothing

---
+++ src/main/java/com/OTPVerification/OTP/Verification/App/Controllers/AuthenticationController.java
@@ -0,0 +1,16 @@
+package com.OTPVerification.OTP.Verification.App.Controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("adminAuth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+
+
+}
