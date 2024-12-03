aad0397dfcd57c329e2f924575376ca38ef9f100
AyushInKC <sirfayush1008@gmail.com>
2024-12-03 21:36:03 +0530

    Project Setup

---
+++ src/main/java/com/OTPVerification/OTP/Verification/App/VerifyByOtpApplication.java
@@ -0,0 +1,13 @@
+package com.OTPVerification.OTP.Verification.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyByOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyByOtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OTPVerification/OTP/Verification/App/VerifyByOtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OTPVerification.OTP.Verification.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyByOtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
